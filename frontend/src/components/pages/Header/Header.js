36fedd43094380296010dc6494c3fe889799005b
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2023-01-24 18:55:49 +0530

    navbar responsiveness fixed

--- frontend/src/components/pages/Header/Header.js
+++ frontend/src/components/pages/Header/Header.js
@@ -55,123 +55,149 @@ const HeaderSection = () => {
 
   return (
     <div className="shadow-sm bg-white rounded">
-      <div className="container-fluid my-3">
-        <div className="row px-xl-5">
-          <div className="col-lg-3">
-            <Link to="" className="text-decoration-none d-none d-lg-block">
-              <h1 className="m-0" style={styles}>
-                <span className="logo">
-                  <span className="ml-2">
-                    <img src="/images/logo.png" height={60} width={60} alt="" />
-                  </span>
-                </span>
-                {/* <span className="text-yellow">Let's </span> */}
-                <span style={{fontSize:"30px" ,backgroundImage:"linear-gradient(60deg, #E21143, #FFB03A)",WebkitBackgroundClip:"text",color:"transparent" ,textTransform:"none"}}>Warelogg</span>
-              </h1>
-            </Link>
-          </div>
-          <div className="col-lg-9">
-            <nav className="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0">
-              <span className="logo-sm">
-                <span className="ml-2">
-                  <img src="/images/logo.png" height={60} width={60} alt="" />
-                </span>
-              </span>
-              <Link
-                to="/"
-                className="text-decoration-none d-block d-lg-none d-xs-none"
-              >
-                <h1 className="m-0">
-                  {/* <span className="text-yellow">Let's </span> */}
-                  <span className="text-orange">Warelogg </span>
-                  {/* <span className="text-orange">Pal</span> */}
-                </h1>
+      <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-light navbar-light py-2 py-lg-0">
+          <Link to="/" className="text-decoration-none d-flex justify-content-center align-items-center">
+            <span>
+              <img src="/images/logo.png" height={60} width={60} alt="" />
+            </span>
+            <span
+              style={{
+                fontSize: "35px",
+                backgroundImage: "linear-gradient(60deg, #E21143, #FFB03A)",
+                WebkitBackgroundClip: "text",
+                color: "transparent",
+                textTransform: "none",
+              }}
+              className="ml-2 d-none d-sm-block"
+            >
+              Warelogg
+            </span>
+          </Link>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-between mx-0 mx-lg-3"
+            id="navbarCollapse"
+          >
+            <div className="navbar-nav py-0">
+              <Link to="/partner" className="nav-item nav-link">
+                List Your Space
               </Link>
-              <button
-                type="button"
-                className="navbar-toggler"
-                data-toggle="collapse"
-                data-target="#navbarCollapse"
+              <Link to="/locations" className="nav-item nav-link">
+                Track & Trace
+              </Link>
+              <Link to="/contact" className="nav-item nav-link">
+                Contact
+              </Link>
+            </div>
+            <div>
+            <Link
+              to="/cart"
+              className="nav-item nav-link cart position-relative d-inline-flex my-2 my-lg-0"
+            >
+              <span className="cart-basket d-flex align-items-center justify-content-center">
+                {ctx.cartValue}
+              </span>
+              <i className="fas fa fa-shopping-cart fa-lg text-dark"></i>
+            </Link>
+            <Dropdown xs={2} className="d-lg-none d-block mx-2" variant="secondary">
+              <Dropdown.Toggle
+                id="dropdown-autoclose-true dropdownHeader"
+                variant="secondary"
+                className="rounded-pill"
               >
-                <span className="navbar-toggler-icon" />
-              </button>
-              <div
-                className="collapse navbar-collapse justify-content-between"
-                id="navbarCollapse"
-              >
-                <div className="navbar-nav py-0">
-                  {/* <Link to="/" className="nav-item nav-link active">
-                    Home
-                  </Link> */}
-                  <Link to="/partner" className="nav-item nav-link">
-                    List Your Space
-                  </Link>
-                  <Link to="/locations" className="nav-item nav-link">
-                    Track & Trace
-                  </Link>
-                  <Link to="/contact" className="nav-item nav-link">
-                    Contact
-                  </Link>
-                </div>
-                <Link
-                  to="/cart"
-                  className="nav-item nav-link cart position-relative d-inline-flex"
-                >
-                  <span className="cart-basket d-flex align-items-center justify-content-center">
-                    {ctx.cartValue}
-                  </span>
-                  <i className="fas fa fa-shopping-cart fa-lg text-dark"></i>
+                <FcMenu />
+                <img
+                  src="/images/avtar.png"
+                  className="rounded-circle mx-2"
+                  height="25px"
+                  alt=""
+                />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Link to="/profile" className="nav-item nav-link">
+                  <Dropdown.Item as="button">Profile</Dropdown.Item>
                 </Link>
-              </div>
-              <Row>
-                <Dropdown xs={2} className="d-inline mx-2" variant="secondary">
-                  <Dropdown.Toggle
-                    id="dropdown-autoclose-true dropdownHeader"
-                    variant="secondary"
-                    className="rounded-pill"
+                <Link to="/orders" className="nav-item nav-link">
+                  <Dropdown.Item as="button">My Bookings</Dropdown.Item>
+                </Link>
+                <Link to="/allListedSpace" className="nav-item nav-link">
+                  <Dropdown.Item as="button">My Listed Space</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+                {usertoken != null ? (
+                  <Button
+                    className="btn btn-dark text-white d-lg-block mx-auto"
+                    onClick={handleLogout}
                   >
-                    <FcMenu />
-                    <img
-                      src="/images/avtar.png"
-                      className="rounded-circle mx-2"
-                      height="25px"
-                      alt=""
-                    />
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Link to="/profile" className="nav-item nav-link">
-                      <Dropdown.Item as="button">Profile</Dropdown.Item>
+                    Logout
+                  </Button>
+                ) : (
+                  <>
+                    <Link to="/register" className="nav-item nav-link">
+                      <Dropdown.Item as="button">Signup</Dropdown.Item>
                     </Link>
-                    <Link to="/orders" className="nav-item nav-link">
-                      <Dropdown.Item as="button">My Bookings</Dropdown.Item>
+                    <Link to="/login" className="nav-item nav-link">
+                      <Dropdown.Item as="button">Login</Dropdown.Item>
                     </Link>
-                    <Link to="/allListedSpace" className="nav-item nav-link">
-                      <Dropdown.Item as="button">My Listed Space</Dropdown.Item>
-                    </Link>
-                    <Dropdown.Divider />
-                    {usertoken != null ? (
-                      <Button
-                        className="btn btn-dark text-white d-lg-block mx-auto"
-                        onClick={handleLogout}
-                      >
-                        Logout
-                      </Button>
-                    ) : (
-                      <>
-                        <Link to="/register" className="nav-item nav-link">
-                          <Dropdown.Item as="button">Signup</Dropdown.Item>
-                        </Link>
-                        <Link to="/login" className="nav-item nav-link">
-                          <Dropdown.Item as="button">Login</Dropdown.Item>
-                        </Link>
-                      </>
-                    )}
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Row>
-            </nav>
+                  </>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+            </div>
           </div>
-        </div>
+          <Dropdown xs={2} className="d-none d-lg-inline" variant="secondary">
+              <Dropdown.Toggle
+                id="dropdown-autoclose-true dropdownHeader"
+                variant="secondary"
+                className="rounded-pill"
+              >
+                <FcMenu />
+                <img
+                  src="/images/avtar.png"
+                  className="rounded-circle mx-2"
+                  height="25px"
+                  alt=""
+                />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Link to="/profile" className="nav-item nav-link">
+                  <Dropdown.Item as="button">Profile</Dropdown.Item>
+                </Link>
+                <Link to="/orders" className="nav-item nav-link">
+                  <Dropdown.Item as="button">My Bookings</Dropdown.Item>
+                </Link>
+                <Link to="/allListedSpace" className="nav-item nav-link">
+                  <Dropdown.Item as="button">My Listed Space</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+                {usertoken != null ? (
+                  <Button
+                    className="btn btn-dark text-white d-lg-block mx-auto"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Button>
+                ) : (
+                  <>
+                    <Link to="/register" className="nav-item nav-link">
+                      <Dropdown.Item as="button">Signup</Dropdown.Item>
+                    </Link>
+                    <Link to="/login" className="nav-item nav-link">
+                      <Dropdown.Item as="button">Login</Dropdown.Item>
+                    </Link>
+                  </>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+        </nav>
       </div>
     </div>
   );
